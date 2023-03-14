9ffe31b025f4086860208c1f9bde7e72baacca0a
sonnguyen16 <sonnv2212@gmail.com>
2023-03-14 19:13:16 +0700

    first commit

---
+++ HandmadeShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HandmadeShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
